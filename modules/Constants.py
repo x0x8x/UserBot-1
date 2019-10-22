997381366d79fd0465f0e982422818e2f118c5c4
Giulio Coa <giulioc008@gmail.com>
2019-10-22 20:31:14 +0200

    Add files via upload

--- modules/Constants.py
+++ modules/Constants.py
@@ -51,12 +51,12 @@ class Constants:
             Reading the database
         """
         with open("database.json", "r") as users:
-            file = json.load(users)
-            """
-                Setting the database
-            """
-            self.__botAdmins = pandas.DataFrame(data=file["admins"], columns=list(["id", "name"]))
-            self.__chat = pandas.DataFrame(data=file["chat"], columns=list(["id", "name"]))
+            file = json.load(users.read())
+		"""
+			Setting the database
+		"""
+		self.__botAdmins = pandas.DataFrame(data=file["admins"], columns=list(["id", "name"]))
+		self.__chat = pandas.DataFrame(data=file["chat"], columns=list(["id", "name"]))
         """
             Setting the parameters
         """
